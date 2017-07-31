045ca4c66f066ecac461430ef7d7365dd51a17a5
CDnX <ruby3141@gmail.com>
2017-08-01 00:53:40 +0900

    Add gfl_pequip on gfl module
     - gfl_pequip get time as argument and offer info about equipment will they get.
      * this commit requires data.db to be up to date. use drop.sql and scheme.sql.

--- module/gfl/__init__.py
+++ module/gfl/__init__.py
@@ -53,9 +53,29 @@ def gfl_produce(pmsg):
 commands.append(([(1,r"PRIVMSG"), \
 	(-1, r"(?i):\./(gfl|소녀전선|소전) (produce|prod|제조)( .*)?")], gfl_produce))
 
+def gfl_pequip(pmsg):
+	if re.fullmatch(r"(?i):\./(gfl|소녀전선|소전) (equip|장비) [0-9]{0,2}", \
+		pmsg[-1]) is not None:
+		time = int(pmsg[-1].split()[-1])
+		c = db.cursor()
+		c.execute("SELECT * from gfl_pequip where time = ?", (time,))
+		dbrs = c.fetchall()
+		if dbrs is not None and len(dbrs) != 0:
+			result = []
+			for dbr in dbrs:
+				result.append(":★%d: %s"%(dbr[1],dbr[2]))
+		else:
+			result = [":해당하는 결과가 없어요!"]
+		return result
+	c = pmsg[-1].split()
+	return [":'%s %s 분'처럼 입력해 주세요!"%(c[0][1:],c[1])]
+
+commands.append(([(1,r"PRIVMSG"), \
+	(-1, r"(?i):\./(gfl|소녀전선|소전) (equip|장비)( .*)?")], gfl_pequip))
+
 def gfl_help(pmsg):
 	return [":소녀전선 관련 정보를 제공하는 명령이에요.", \
-	":./(gfl|소녀전선|소전) (exp|경험치|경)|(produce|prod|제조)"]
+	":./(gfl|소녀전선|소전) (exp|경험치|경)|(produce|prod|제조)(equip|장비)"]
 
 commands.append(([(1,r"PRIVMSG"), \
 	(-1, r"(?i):\./(gfl|소녀전선|소전) ?")], gfl_help))
